2e871ebe79a776c220cf915b62d69fb7b03bb91a
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2018-10-24 09:41:09 +0200

    traces

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ serverless slstats -d
 npm install -g @ibm-functions/shell --unsafe-perm
 
 # wskdeploy
-curl -LO $(curl --silent "https://api.github.com/repos/apache/incubator-openwhisk-wskdeploy/releases/latest" | jq -r .assets[].browser_download_url | grep linux-amd64) \
+curl -LO $(curl --retry 10 --retry-delay 5 --silent "https://api.github.com/repos/apache/incubator-openwhisk-wskdeploy/releases/latest" | jq -r .assets[].browser_download_url | grep linux-amd64) \
   && tar zxvf openwhisk_wskdeploy*.tgz wskdeploy \
   && mv wskdeploy /usr/local/bin/ \
   && rm -f openwhisk_wskdeploy*.tgz
@@ -23,11 +23,13 @@ curl -LO $(curl --silent "https://api.github.com/repos/apache/incubator-openwhis
 pip install softlayer
 
 # Bluemix CLI
+echo ">> ibmcloud"
 curl -fsSL https://clis.ng.bluemix.net/install/linux > /tmp/bxinstall.sh
 sh /tmp/bxinstall.sh
 rm /tmp/bxinstall.sh
 
 # Bluemix CLI plugins
+echo ">> ibmcloud plugins"
 bx plugin install activity-tracker -f -r Bluemix
 bx plugin install cloud-functions -f -r Bluemix
 bx plugin install cloud-internet-services -r Bluemix
@@ -38,36 +40,53 @@ bx plugin install infrastructure-service -r Bluemix
 bx plugin install logging-cli -r Bluemix
 
 # Kubernetes
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
-  mv kubectl /usr/local/bin/kubectl && \
-  chmod +x /usr/local/bin/kubectl
+echo ">> kubectl"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl --retry 10 --retry-delay 5 -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl \
+  && mv kubectl /usr/local/bin/kubectl \
+  && chmod +x /usr/local/bin/kubectl
 
 # Kubetail
-curl -LO https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail && \
-  mv kubetail /usr/local/bin/kubetail && \
-  chmod +x /usr/local/bin/kubetail
+echo ">> kubetail"
+curl -LO https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail \
+  && mv kubetail /usr/local/bin/kubetail \
+  && chmod +x /usr/local/bin/kubetail
 
 # Kail https://github.com/boz/kail
-curl -LO $(curl --silent "https://api.github.com/repos/boz/kail/releases/latest" | jq -r .assets[].browser_download_url | grep linux_amd64) \
+echo ">> kail"
+curl -LO $(curl --retry 10 --retry-delay 5 --silent "https://api.github.com/repos/boz/kail/releases/latest" | jq -r .assets[].browser_download_url | grep linux_amd64) \
   && mkdir kail \
   && tar zxvf kail*linux*.tar.gz -C kail \
   && mv kail/kail /usr/local/bin/kail \
   && rm -rf kail kail*linux*.tar.gz
 
 # Istio
-curl -LO $(curl --silent "https://api.github.com/repos/istio/istio/releases/latest" | jq -r .assets[].browser_download_url | grep linux) \
+echo ">> istio"
+curl -LO $(curl --retry 10 --retry-delay 5 --silent "https://api.github.com/repos/istio/istio/releases/latest" | jq -r .assets[].browser_download_url | grep linux) \
   && tar zxvf istio-*.tar.gz \
   && rm -f istio-*.tar.gz \
   && mv istio-* /usr/local/ \
   && ln -s /usr/local/istio* /usr/local/istio
 
 # Knative CLI https://github.com/cppforlife/knctl
-curl -LO $(curl --silent "https://api.github.com/repos/cppforlife/knctl/releases/latest" | jq -r .assets[].browser_download_url | grep linux-amd64) \
+echo ">> knctl"
+curl -LO $(curl --retry 10 --retry-delay 5 --silent "https://api.github.com/repos/cppforlife/knctl/releases/latest" | jq -r .assets[].browser_download_url | grep linux-amd64) \
   && mv knctl* /usr/local/bin/knctl \
   && chmod +x /usr/local/bin/knctl
 
 # Helm
+echo ">> helm"
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+
+# IBM provider for Terraform
+echo ">> terraform"
+curl -LO $(curl --retry 10 --retry-delay 5 -I https://github.com/IBM-Cloud/terraform-provider-ibm/releases/latest | grep Location | awk '{print $2}' | sed 's/tag/download/g' | tr -d '[:space:]')/linux_amd64.zip && \
+  unzip linux_amd64.zip && \
+  rm -f linux_amd64.zip && \
+  mkdir /usr/local/share/terraform && \
+  mv terraform-provider-ibm* /usr/local/share/terraform/terraform-provider-ibm && \
+  echo 'providers {
+  ibm = "/usr/local/share/terraform/terraform-provider-ibm"
+}' > /root/.terraformrc
 
 # Expose configuration to be overriden
 mkdir -p /root/mnt/config
@@ -102,16 +121,6 @@ ln -s /root/mnt/config/slcli.conf /root/.softlayer
 # Bluemix SoftLayer service
 mkdir /root/mnt/config/softlayer
 ln -s /root/mnt/config/softlayer /root/.bluemix/plugins/softlayer
-
-# IBM provider for Terraform
-curl -LO $(curl -I https://github.com/IBM-Cloud/terraform-provider-ibm/releases/latest | grep Location | awk '{print $2}' | sed 's/tag/download/g' | tr -d '[:space:]')/linux_amd64.zip && \
-  unzip linux_amd64.zip && \
-  rm -f linux_amd64.zip && \
-  mkdir /usr/local/share/terraform && \
-  mv terraform-provider-ibm* /usr/local/share/terraform/terraform-provider-ibm && \
-  echo 'providers {
-  ibm = "/usr/local/share/terraform/terraform-provider-ibm"
-}' > /root/.terraformrc
 
 # Helm configuration
 mkdir /root/mnt/config/helm
